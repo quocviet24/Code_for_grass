d00ac2ba5f52c4acfcd21f486390ffaeb21a07e5
nishikatakagi <quocviet242003@gmail.com>
2024-09-11 20:16:58 +0700

    update heart 3 d

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 function openLetter() {
   const envelope = document.querySelector('.envelope');
   const promptText = document.getElementById('promptText');
+  const body = document.querySelector('body');
+  const heart = document.querySelector('.heart');
   
   envelope.classList.toggle('open');
   
-  // Ẩn dòng chữ "Hãy ấn vào lá thư đi" khi bức thư mở
   if (envelope.classList.contains('open')) {
-      promptText.classList.add('hidden');
+    promptText.classList.add('hidden');
+    heart.style.opacity = '1';
+    
+    setTimeout(function() {
+      body.classList.add('fadeout');
+      setTimeout(function() {
+        window.location.href = 'heart.html';
+      }, 1000); // Thời gian cho hiệu ứng fade-out hoàn tất
+    }, 5000); // Thời gian chờ trước khi bắt đầu hiệu ứng
+  } else {
+    heart.style.opacity = '0';
   }
 }
